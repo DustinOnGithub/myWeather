95a1bad3d15443c51daa7afee04e61eaddcd39c6
Dustin <dustin@heiseweb.de>
2019-11-26 17:55:40 +0100

    webview debugable

--- app/src/main/java/com/example/myweahter/MainActivity.kt
+++ app/src/main/java/com/example/myweahter/MainActivity.kt
@@ -3,9 +3,18 @@ package com.example.myweahter
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.webkit.WebChromeClient
+import android.content.pm.ApplicationInfo
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +24,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        webView = findViewById(R.id.webView);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) {
+                WebView.setWebContentsDebuggingEnabled(true)
+            }
+        }
+
+        webView = findViewById(R.id.webView)
+
+        webView.webChromeClient = object : WebChromeClient() {
+
+            override fun onConsoleMessage(message: String, lineNumber: Int, sourceID: String) {
+                Log.d("MyApplication", "$message -- From line $lineNumber of $sourceID")
+            }
+        }
+
         webView.webViewClient = object : WebViewClient(){
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
                 view?.loadUrl(url)
